55d105096259c00ca1c6bedca7cce7ffb79a3e7f
TERA <p.tyche92@gmail.com>
2026-02-05 11:08:23 +0900

    [mod] shadcn 파일 위치 조정 #2

--- src/containers/login/index.container.tsx
+++ src/containers/login/index.container.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button'
+import { Button } from '@/shared/components/ui'
 import {
   Card,
   CardContent,
@@ -6,9 +6,9 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+} from '@/shared/components/ui'
+import { Input } from '@/shared/components/ui'
+import { Label } from '@/shared/components/ui'
 
 export function LoginContainer() {
   return (
