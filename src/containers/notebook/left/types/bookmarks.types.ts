c2269184e0cfa6b041db5b5849b18c1b5ac61ffc
reasonableplan <juwon123465@gmail.com>
2026-03-20 16:47:35 +0900

    feat: 소스 전체 선택/해제 체크박스 깜빡임 버그 수정 (#82)

--- src/containers/notebook/left/types/bookmarks.types.ts
+++ src/containers/notebook/left/types/bookmarks.types.ts
@@ -31,11 +31,12 @@ export type BookmarkStore = {
   bookmarks: BookmarkState
   rootIds: number[]
   isLoading: boolean
+  isSyncing: boolean
   searchQuery: string
   error: ErrorAlertState | null
   fetchAndInitialize: (notebookId: number) => Promise<void>
   toggleExpand: (id: number) => void
-  toggleCheck: (id: number, isChecked: boolean) => void
+  toggleCheck: (id: number, isChecked: boolean) => Promise<void>
   toggleCheckAll: (isChecked: boolean) => Promise<void>
   getCheckedSources: () => CheckedSource[]
   deleteBookmark: (id: number) => Promise<void>
